d0a0a4b42d545eefd785ad883256a77268daa732
DirtyDan00 <47438008+DirtyDan00@users.noreply.github.com>
2019-11-25 05:58:51 -0700

    Update PokedexObj.java

--- PokeDexCreator/src/main/java/vella/j/PokeDexCreator/PokedexObj.java
+++ PokeDexCreator/src/main/java/vella/j/PokeDexCreator/PokedexObj.java
@@ -1,5 +1,39 @@
 package vella.j.PokeDexCreator;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Pokedex")
 public class PokedexObj {
 
+	@Id
+	private int pokedexId;
+	@OneToOne//maybe one to one?
+	private PokemonObj pokemonData;
+	public int getPokedexId() {
+		return pokedexId;
+	}
+	public void setPokedexId(int pokedexId) {
+		this.pokedexId = pokedexId;
+	}
+	public PokemonObj getPokemonData() {
+		return pokemonData;
+	}
+	public void setPokemonData(PokemonObj pokemonData) {
+		this.pokemonData = pokemonData;
+	}
+	public PokedexObj(int pokedexId, PokemonObj pokemonData) {
+		super();
+		this.pokedexId = pokedexId;
+		this.pokemonData = pokemonData;
+	}
+	public PokedexObj() {
+		
+	}
+	
+	
+	
 }
